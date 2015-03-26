5af2b454bc2a1a4e1f4eedabf282fa0ca8953759
Dale Sikkema <dsikkema@ebay.com>
2015-03-26 00:53:08 -0500

    MAGETWO-32793: creation of m2-sample-interception extension

--- sample-module-interception/Controller/Index/Index.php
+++ sample-module-interception/Controller/Index/Index.php
@@ -8,6 +8,9 @@ namespace Magento\SampleInterception\Controller\Index;
 
 /**
  * Loads the demo page.
+ *
+ * This is a basic controller that only loads the corresponding layout file. It may duplicate other such 
+ * controllers, and thus it is considered tech debt. This code duplication will be resolved in future releases.
  */
 class Index extends \Magento\Framework\App\Action\Action
 {
